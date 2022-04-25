b5c6433884b8a820da8e249825eb73727533607c
J0v1 <67576010+J0v1-b@users.noreply.github.com>
2022-04-25 10:27:47 -0300

    Create usersheets.py

---
+++ usersheets.py
@@ -0,0 +1,53 @@
+import csv
+ 
+def print_users():
+    with open("users.csv") as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        for row in csv_reader: 
+            print("{} {}".format(row['First Name'], row['Last Name']))
+            
+def find_user(first_name, last_name):
+    with open("users.csv") as csvfile:
+        csv_reader = csv.reader(csvfile)
+        for (index, row) in enumerate(csv_reader):
+            first_name_match = first_name == row[0]
+            last_name_match = last_name == row[1]
+            if first_name_match and last_name_match:
+                return index
+        return "{} {} not found.".format(first_name, last_name)
+      
+def update_users(old_first, old_last, new_first, new_last):
+    with open("users.csv") as csvfile:
+        csv_reader = csv.reader(csvfile)
+        rows = list(csv_reader)
+ 
+    count = 0
+    with open("users.csv", "w") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        for row in rows:
+            if row[0] == old_first and row[1] == old_last:
+                csv_writer.writerow([new_first, new_last])
+                count += 1
+            else:
+                csv_writer.writerow(row)
+ 
+    return "Users updated: {}.".format(count)
+
+#Delete users
+import csv
+ 
+def delete_users(first_name, last_name):
+    with open("users.csv") as csvfile:
+        csv_reader = csv.reader(csvfile)
+        rows = list(csv_reader)
+ 
+    count = 0
+    with open("users.csv", "w") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        for row in rows:
+            if row[0] == first_name and row[1] == last_name:
+                count += 1
+            else:
+                csv_writer.writerow(row)
+ 
+    return "Users deleted: {}.".format(count)      
